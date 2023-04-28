c005b49a575a850a474a58707144c1404335d233
Pinkeshchoudhary2009 <122616889+Pinkeshchoudhary2009@users.noreply.github.com>
2023-04-28 18:00:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+/*
+    Use beginPath() function to begin a path.
+    Then, use strokeStyle to set the color to "grey".
+    Use lineWidth and set the width to 1.
+    Use rect() to create a rectangle at 150 and 143
+with 430 width and 200 height.
+*/
+
+ctx.grey;
+ctx.color = "grey";
+ctx.width = 1;
+ctx.rec=(x, y, width, height);
+ctx.stroke();
+
+/*
+    Use beginPath() function to begin a path.
+    Then, use strokeStyle to set the color to "blue".
+    Use lineWidth and set the width to 5.
+    Use arc() to create a circle at x = 250 and y = 210
+with 40 as radius, 0 startAngle and 2 * Math.PI endAngle.
+*/
+
+ctx.blue;
+ctx.color = "blue";
+ctx.width = 5;
+ctx.arc(circle= x=250, y=210, r=40, 0=startAngle=2* Math.PI endAngle);
+ctx.stroke();
+
+// Similarly, create a black circle with position 350 and 210
+ctx.black;
+ctx.color = "black";
+ctx.width = 5;
+ctx.arc(x=350, y=210, r=40, 0=startAngle,Math.PI endAngle);
+ctx.stroke();
+// Similarly, create a red circle with position 210 and 40
+ctx.red;
+ctx.color = "red";
+ctx.width = 5;
+ctx.arc(x=350, y=210, r=40, 0=startAngle,Math.PI endAngle);
+ctx.stroke();
+// Similarly, create an orange circle with position 300 and 250
+ctx.orange;
+ctx.color = "orange";
+ctx.width = 5;
+ctx.arc(x=350, y=210, r=40, 0=startAngle,Math.PI endAngle);
+ctx.stroke();
+// Similarly, create a green circle with position 400 and 250
+ctx.black;
+ctx.color = "green";
+ctx.width = 5;
+ctx.arc(x=350, y=210, r=40, 0=startAngle,Math.PI endAngle);
+ctx.stroke();
